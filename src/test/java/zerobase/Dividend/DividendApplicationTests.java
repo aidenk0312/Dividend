c0cfefbbe03aa5ee9f8c6e8b5e317d91999b177a
yongsuKim <yongsu0312.kim@gmail.com>
2023-03-24 21:05:19 +0900

    2303242105

---
+++ src/test/java/zerobase/Dividend/DividendApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.Dividend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
